a581cc8b50bcf45d84375e0d0df92daa73b7f247
Lucas Matias <101410443+MatiasDeLima@users.noreply.github.com>
2023-11-20 17:21:50 -0300

    feat(examples): updata examples

--- examples/index.js
+++ examples/index.js
@@ -19,17 +19,18 @@ const header = new SketchUI.Header(headerConfig);
 header.createHeader();
 
 const formConfig = {
-    inputs: [
-        { icon: "ri-user-3-line", type: "name", placeholder: "Enter your username..." },
-        { icon: "ri-mail-line", type: "email", placeholder: "Enter Your email..." },
-        { icon: "ri-lock-2-line", type: "password", placeholder: "Enter Your password..." },
+    formInputs: [
+        { icon: "ri-user-3-line", type: "name", placeholder: "Enter your username...", id: "form-name"},
+        { icon: "ri-mail-line", type: "email", placeholder: "Enter Your email...", id: "form-email"},
+        { icon: "ri-lock-2-line", type: "password", placeholder: "Enter Your password...", id: "form-password" },
     ],
     formTitle: "Criar Conta",
     formTerms: "You accept the terms and coditions?",
     formError: "An error occured!",
-    formButton: "Registrar",
+    formButton: "Sign Up",
     formText: "Already have an account?",
-    formLink: "Entrar"
+    formLink: "Sign In",
+    formPath: "/register"
 };
 
 const form = new SketchUI.Form(formConfig);
